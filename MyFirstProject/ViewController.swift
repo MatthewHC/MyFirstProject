1d4937025b6b5c744ee4766134fcb7eadc049af6
Matthew Claydon <mclaydon@bigpond.net.au>
2023-05-21 19:46:51 +1000

    Another try

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // We will start working on the initial login feature
         //This is the second test to try and get source control to send to GIT Hub
+        // Here is a second change to see that is happening with sourcetree and GitHub
+        //Seems easy to get confused and make a mess!
+        
         
     
     }
