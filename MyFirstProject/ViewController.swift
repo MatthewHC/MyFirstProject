97f75aac40ea98ad06652b9e2d5afe5b140651f3
Matthew Claydon <mclaydon@bigpond.net.au>
2023-05-21 19:27:26 +1000

    This is the second test to see how quickly this communicates with GitHub

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // We will start working on the initial login feature
         //This is the second test to try and get source control to send to GIT Hub
+        // Here is another change to try and workout how the sourcetree program works
+    
         
     
     }
