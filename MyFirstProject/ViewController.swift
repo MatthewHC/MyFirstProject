229fc56c79c2b4c186d99de8f6c4fbf42cd4a02d
Matthew Claydon <mclaydon@bigpond.net.au>
2023-05-21 18:38:32 +1000

    Initial an iOS project

---
+++ MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Matthew Claydon on 21/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
